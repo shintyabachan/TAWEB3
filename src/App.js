b84201d1c43f3162f87d5e2b6208079f4255a881
shintyabachan <icinbachan15@gmail.com>
2019-05-17 10:43:45 +0700

    push1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainMenu from './component/MainMenu';
+import { Row } from 'antd';
+implements MainRoute from '/components/page/MainRoute';
+import {};
+
+function App() {
+  return (
+    <div>
+      <MainMenu>
+        <Row>
+          
+        </Row>
+      </MainMenu>
+    </div>
+  );
+}
+
+export default App;
